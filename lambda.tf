c593eee3b347c70eb54d33ad03165564dc74b545
“sivad1648” <sivad1648@gmail.com>
2022-07-20 00:15:58 +0530

    ut

--- lambda.tf
+++ lambda.tf
@@ -1,9 +1,16 @@
-import pathlib{ 
+locals {
+    lambda_zip_location = "./lambda_function.zip"
+}
+data "archive_file" "lambda_function" {
+  type        = "zip"
+  source_file = "lambda_function.py"
+  output_path = "${local.lambda_zip_location}"
+}
 resource "aws_lambda_function" "test_lambda" {
   # If the file is not in the current working directory you will need to include a 
   # path.module in the filename.
-  path          = "/home/ubuntu/jenkins/workspace/Lambda_deploy_via_terraform"
-  filename      = "lambda_function.zip"
+  
+  filename      = "${local.lambda_zip_location}"
   function_name = "lambda_function"
   role          = aws_iam_role.lambda_role.arn
   handler       = "lambda_function.lambda_handler"
@@ -15,5 +22,4 @@ resource "aws_lambda_function" "test_lambda" {
 
   runtime = "python3.8"
 
-}
 }
